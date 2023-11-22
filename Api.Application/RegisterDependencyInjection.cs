7a68ad98fb5072cc53d2aa3004ad3a8e0a7661b7
Gerald Ncube <gerald.ncube@se.com>
2023-11-22 23:27:52 +0000

    Added Domain and Application layers

---
+++ Api.Application/RegisterDependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Application;
+
+public static class RegisterDependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
