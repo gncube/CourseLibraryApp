82647f4864fd1162af5c30a7a8036f3fefc8719c
Gerald Ncube <gerald.ncube@se.com>
2023-11-23 11:14:21 +0000

    Added LocalStorageConstants to Api.Infrastructure

---
+++ src/Api.Application/Helpers/LocalStorageConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace Api.Application.Helpers;
+public class LocalStorageConstants
+{
+    public const string AuthorListExpirationKey = "AuthorListExpiration";
+    public const string AuthorListKey = "AuthorList";
+}
--- src/Client.Infrastructure/DataServices/AuthorDataService.cs
+++ src/Client.Infrastructure/DataServices/AuthorDataService.cs
@@ -1,4 +1,6 @@
-﻿using Api.Domain;
+﻿using Api.Application.Helpers;
+using Api.Domain;
+using Blazored.LocalStorage;
 using Client.Application.Contracts;
 using Microsoft.Extensions.Logging;
 using System.Text.Json;
@@ -8,27 +10,68 @@ public class AuthorDataService : IDataService<Author, Guid>
 {
     private readonly HttpClient _httpClient;
     private readonly JsonSerializerOptions _jsonSerializerOptions;
+    private readonly ILocalStorageService _localStorageService;
     private readonly ILogger _logger;
 
-    public AuthorDataService(HttpClient httpClient, JsonSerializerOptions jsonSerializerOptions, ILoggerFactory loggerFactory)
+    public AuthorDataService(HttpClient httpClient, JsonSerializerOptions jsonSerializerOptions, ILocalStorageService localStorageService, ILoggerFactory loggerFactory)
     {
         _httpClient = httpClient;
         _jsonSerializerOptions = jsonSerializerOptions;
+        _localStorageService = localStorageService;
         _logger = loggerFactory.CreateLogger<AuthorDataService>();
     }
     public async Task<IEnumerable<Author>> GetAllAsync()
     {
-        using var responseStream = await _httpClient.GetStreamAsync("api/authors");
-        using var streamReader = new StreamReader(responseStream);
-        var authorsFromApi = await JsonSerializer.DeserializeAsync<IEnumerable<Author>>(streamReader.BaseStream, _jsonSerializerOptions);
+        var refreshRequired = false;
 
-        if (authorsFromApi is null)
+        try
         {
-            _logger.LogError("Could not deserialize authors from API");
-            throw new Exception("Could not deserialize authors from API");
-        }
+            if (!refreshRequired)
+            {
+                bool authorListExpirationKeyExists = await _localStorageService.ContainKeyAsync(LocalStorageConstants.AuthorListExpirationKey);
+                if (authorListExpirationKeyExists)
+                {
+                    var expirationDate = await _localStorageService.GetItemAsync<DateTime>(LocalStorageConstants.AuthorListExpirationKey);
+                    if (expirationDate > DateTime.Now)
+                    {
+                        if (await _localStorageService.ContainKeyAsync(LocalStorageConstants.AuthorListKey))
+                        {
+                            return await _localStorageService.GetItemAsync<IEnumerable<Author>>(LocalStorageConstants.AuthorListKey);
+                        }
+                    }
+                }
+            }
 
-        return authorsFromApi;
+            using var responseStream = await _httpClient.GetStreamAsync("api/authors");
+            using var streamReader = new StreamReader(responseStream);
+            var authorsFromApi = await JsonSerializer.DeserializeAsync<IEnumerable<Author>>(streamReader.BaseStream, _jsonSerializerOptions);
+
+            if (authorsFromApi is null)
+            {
+                _logger.LogError("Could not deserialize authors from API");
+                throw new Exception("Could not deserialize authors from API");
+            }
+
+            await _localStorageService.SetItemAsync(LocalStorageConstants.AuthorListKey, authorsFromApi);
+            await _localStorageService.SetItemAsync(LocalStorageConstants.AuthorListExpirationKey, DateTime.Now.AddMinutes(5));
+
+            return authorsFromApi;
+        }
+        catch (HttpRequestException ex)
+        {
+            _logger.LogError(ex, "Error in {FunctionName} getting authors from API", nameof(GetAllAsync));
+            return Enumerable.Empty<Author>();
+        }
+        catch (JsonException ex)
+        {
+            _logger.LogError(ex, "Could not deserialize authors from API");
+            return Enumerable.Empty<Author>();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in {FunctionName} getting authors from API", nameof(GetAllAsync));
+            return Enumerable.Empty<Author>();
+        }
     }
 
     public Task<Author> GetAsync(Guid id)
